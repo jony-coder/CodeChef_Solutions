242ee517c9374ff8be06dde0d53ee86824004262
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-05-27 22:06:52 +0600

    Add files via upload

---
+++ CC_FLOW001.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int T;
+    int a,b,sum;
+    cin>>T;
+
+    for(int i=0; i<T; i++){
+
+        cin>>a>>b;
+        sum = a + b;
+        cout<<sum<<endl;
+
+    }
+
+return 0;
+}
