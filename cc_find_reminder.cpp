c94383e91f187bd60cfbdadf3dad4b1f7225d48d
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-06-17 12:00:26 +0600

    Add files via upload

---
+++ cc_find_reminder.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a,b,T;
+    cin>>T;
+    for(int i=0; i<T; i++){
+        int mod = 0;
+        cin>>a>>b;
+
+        mod = a%b;
+        cout<<mod<<endl;
+    }
+
+
+
+
+return 0;
+}
